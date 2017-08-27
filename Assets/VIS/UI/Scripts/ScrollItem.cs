12d878f8dedc51a818fda69c8051c816f8e34aab
TOSHIKI-PC\Imagawa <comcreate.jp@gmail.com>
2017-08-28 01:12:39 +0900

    一旦保存

---
+++ Assets/VIS/UI/Scripts/ScrollItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace VIS.UI
+{
+    public class ScrollItemBehaviour : UIBehaviour
+    {
+        public void UpdateItem<T>(T item) where T : ScrollItem
+        {
+            var rect = transform as RectTransform;
+            rect.sizeDelta = item.SizeDelta;
+        }
+    }
+    public class ScrollItem
+    {
+        public Vector2 SizeDelta { get; private set; }
+        public ScrollItem(Vector2 _sizeDelta)
+        {
+            SizeDelta = _sizeDelta;
+        }
+    }
+    public interface IScrollItemSetUp
+    {
+        void UpdateItem<T>(int count,T item, GameObject obj) where T : ScrollItem;
+    }
+}
