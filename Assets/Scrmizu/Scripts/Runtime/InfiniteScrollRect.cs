85894f58360b10621d4bd19f1bfd6c964118df5e
Toshiki Imagawa <shibainusamulai@gmail.com>
2019-01-01 21:43:17 +0900

    [update] 非公開コンポーネント移動

--- Assets/Scrmizu/Scripts/Runtime/InfiniteScrollRect.cs
+++ Assets/Scrmizu/Scripts/Runtime/InfiniteScrollRect.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 namespace Scrmizu
 {
-    [AddComponentMenu("UI/Infinite Scroll")]
+    [AddComponentMenu("UI/Infinite Scroll Rect")]
     public class InfiniteScrollRect : ScrollRect
     {
         /// <summary>
@@ -465,6 +465,57 @@ namespace Scrmizu
             Canvas.ForceUpdateCanvases();
             gameObject.SetActive(false);
             gameObject.SetActive(true);
+        }
+
+        internal class InfiniteScrollBinder : MonoBehaviour
+        {
+            private InfiniteScrollRect _infiniteScroll;
+
+            private RectTransform _rectTransform;
+            private IInfiniteScrollItem _infiniteScrollItem;
+            private int _itemIndex;
+
+            private RectTransform RectTransform =>
+                _rectTransform != null ? _rectTransform : _rectTransform = GetComponent<RectTransform>();
+
+            public Vector2 size = Vector2.zero;
+
+
+            public int ItemIndex => _itemIndex;
+
+            private IInfiniteScrollItem InfiniteScrollItem =>
+                _infiniteScrollItem ?? (_infiniteScrollItem = GetComponent<IInfiniteScrollItem>());
+
+            public void SetInfiniteScroll(InfiniteScrollRect infiniteScroll)
+            {
+                _infiniteScroll = infiniteScroll;
+            }
+
+            public void Hide()
+            {
+                _itemIndex = -1;
+                InfiniteScrollItem.Hide();
+            }
+
+            public void UpdateItemData(object data, int itemIndex)
+            {
+                _itemIndex = itemIndex;
+                InfiniteScrollItem.UpdateItemData(data);
+            }
+
+            public void UpdateItemPosition(Vector2 position)
+            {
+                RectTransform.anchoredPosition = position;
+            }
+
+            public void UpdateSize()
+            {
+                var newSize = new Vector2(RectTransform.rect.width, RectTransform.rect.height);
+                if (newSize == Vector2.zero) return;
+                if (size == newSize) return;
+                size = newSize;
+                _infiniteScroll.UpdateItemSize(this);
+            }
         }
     }
 
