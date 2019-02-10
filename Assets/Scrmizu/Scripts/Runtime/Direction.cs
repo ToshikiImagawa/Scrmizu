c7e590d8f9e6030fa078f346de0672a41715a039
ToshikiImagawa <shibainusamulai@gmail.com>
2019-02-10 14:57:28 +0900

    [update] Diretion分離

---
+++ Assets/Scrmizu/Scripts/Runtime/Direction.cs
@@ -0,0 +1,11 @@
+﻿namespace Scrmizu
+{
+    /// <summary>
+    /// Direction type
+    /// </summary>
+    public enum Direction
+    {
+        Vertical = 0,
+        Horizontal = 1,
+    }
+}
--- Assets/Scrmizu/Scripts/Runtime/InfiniteScrollRect.cs
+++ Assets/Scrmizu/Scripts/Runtime/InfiniteScrollRect.cs
@@ -554,13 +554,4 @@ namespace Scrmizu
             }
         }
     }
-
-    /// <summary>
-    /// Direction type
-    /// </summary>
-    public enum Direction
-    {
-        Vertical = 0,
-        Horizontal = 1,
-    }
 }
