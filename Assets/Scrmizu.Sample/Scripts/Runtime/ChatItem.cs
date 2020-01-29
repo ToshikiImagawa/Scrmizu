74d0895ffe1695d19ebd09b1d661a1f4079a634e
Toshiki Imagawa <shibainusamulai@gmail.com>
2020-01-29 09:24:38 +0900

    [update] チャットサンプル続き

--- Assets/Scrmizu.Sample/Scripts/Runtime/ChatItem.cs
+++ Assets/Scrmizu.Sample/Scripts/Runtime/ChatItem.cs
@@ -2,16 +2,41 @@
 // Copyright (c) 2016-2020 COMCREATE. All rights reserved.
 
 using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scrmizu.Sample
 {
     [RequireComponent(typeof(RectTransform))]
     public class ChatItem : MonoBehaviour, IInfiniteScrollItem
     {
+        private Text _sendTime;
+        private Text _message;
+        private Image _userIcon;
+
+        private Text SendTime => _sendTime != null
+            ? _sendTime
+            : _sendTime = GetComponentsInChildren<Text>()
+                .FirstOrDefault(text => text.name == "SendTime");
+
+        private Text Message => _message != null
+            ? _message
+            : _message = GetComponentsInChildren<Text>()
+                .FirstOrDefault(text => text.name == "Message");
+
+        private Image UserIcon => _userIcon != null
+            ? _userIcon
+            : _userIcon = GetComponentsInChildren<Image>()
+                .FirstOrDefault(text => text.name == "UserIcon");
+
         void IInfiniteScrollItem.UpdateItemData(object data)
         {
             gameObject.SetActive(true);
+            if (!(data is ChatItemData chatItemData)) return;
+            SendTime.text = chatItemData.SendTime.ToString("yyyy/MM/dd HH:mm:ss");
+            Message.text = chatItemData.Message;
+            UserIcon.sprite = Resources.Load<Sprite>($"Textures/{chatItemData.UserId}");
         }
 
         void IInfiniteScrollItem.Hide()
@@ -22,8 +47,15 @@ namespace Scrmizu.Sample
 
     public class ChatItemData
     {
-        public string UserId { get; set; }
-        public string Message { get; set; }
-        public DateTime SendTime { get; set; }
+        public string UserId { get; }
+        public string Message { get; }
+        public DateTime SendTime { get; }
+
+        public ChatItemData(string userId, string message, DateTime sendTime)
+        {
+            UserId = userId;
+            Message = message;
+            SendTime = sendTime;
+        }
     }
 }
